b95aaad40bbd964c8cab2d862c1ac2ccbb5d4b1b
Morgan Schall <morgan.allison.schall@gmail.com>
2021-06-02 15:30:45 -0400

    added another JS file to set up storage

--- public/service-worker.js
+++ public/service-worker.js
@@ -15,3 +15,15 @@ const FILES_TO_CACHE = [
 
 const PRECACHE = 'precache-v1';
 const RUNTIME = 'runtime';
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+      caches
+        .open(PRECACHE)
+        .then((cache) => {
+            console.log('files have been pre-cached');
+            return cache.addAll(FILES_TO_CACHE);
+        })
+        .then(self.skipWaiting())
+    );
+  });
