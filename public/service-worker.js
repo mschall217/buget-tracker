802ae27e760b7b8a86447cde95dadcb83424a797
Morgan Schall <morgan.allison.schall@gmail.com>
2021-06-02 15:14:35 -0400

    added files to cache

---
+++ public/service-worker.js
@@ -0,0 +1,17 @@
+const FILES_TO_CACHE = [
+    `/`,
+    `/index.html`,
+    '/index.js',
+    `/icons/icon-192x192.png`
+    `/icons/icon-512x512.png`
+    `/manifest.webmanifest`
+    `/styles.css`
+    `https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css`
+    `https://cdn.jsdelivr.net/npm/chart.js@2.8.0`
+
+
+]
+
+
+const PRECACHE = 'precache-v1';
+const RUNTIME = 'runtime';
